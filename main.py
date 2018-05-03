b6ad1624c36438591fbac310a3af9a672bb4691e
Owen Maschal <owenmaschal0598@gmail.com>
2018-05-03 15:05:14 -0400

    Commented out print statements.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ def request_imgur_image(file_url):
     regex2 = re.compile('^https://imgur.com/a/([a-zA-Z0-9]*)$')
     m = regex2.match(file_url)
     if m:
-        print('This is an album! We are going to ignore these for now.')
+        #print('This is an album! We are going to ignore these for now.')
     else: # This is a single image
         request_image(file_url + '.jpg')
 
 def request_image(file_url):
-    req = requests.get(file_url, stream=True, timeout=1.0)
+    req = requests.get(file_url, stream=True, timeout=10.0)
     regex_name = re.compile('.*/([a-zA-Z0-9]+(\.[a-zA-Z0-9]{3})?)$')
     id = regex_name.match(file_url).group(1)
     if req.status_code == requests.codes.ok:
@@ -19,14 +19,14 @@ def request_image(file_url):
         req.raise_for_status()
 
 def main():
-    print('Authenticating...')
+    #print('Authenticating...')
     reddit = praw.Reddit('bot1')
-    print('Authentication successful!')
+    #print('Authentication successful!')
     subreddit = reddit.subreddit('wallpapers')
     regex = re.compile('^https://imgur.com/')
     pathlib.Path('./img/').mkdir(parents=True, exist_ok=True)
     for submission in subreddit.top('day', limit=10):
-        print(submission.url)
+        #print(submission.url)
         if regex.match(submission.url):
             request_imgur_image(submission.url)
         else: # Direct image link
