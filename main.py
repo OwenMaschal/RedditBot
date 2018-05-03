beb6f8657aa3ed0ac79fd30a01ab597954135dfb
Owen Maschal <owenmaschal0598@gmail.com>
2018-05-03 00:33:43 -0400

    Get top 10 of day and outputs to out.txt

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import praw
+import datetime, praw
 
+print('Authenticating...')
 reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit('UMD')
+print('Authentication successful!')
 
-#for submission in subreddit.hot(limit=10):
+subreddit = reddit.subreddit('pythonforengineers')
+with open('out.txt', 'a') as f:
+    f.write(str(datetime.datetime.now()) + '\n')
+    for submission in subreddit.top('day', limit=10):
+        f.write('https://www.reddit.com/' + submission.permalink + '\n')
+    f.write('\n')# Add a line between commits
