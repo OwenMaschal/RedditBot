887db2653a9da5b5543d187191941c75efb6f644
Owen Maschal <owenmaschal0598@gmail.com>
2018-05-03 15:04:23 -0400

    Gets top 10 posts of the day. Albums are still not working properly.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-import datetime, praw
+import praw, requests, shutil, re, json, pathlib
 
-print('Authenticating...')
-reddit = praw.Reddit('bot1')
-print('Authentication successful!')
+def request_imgur_image(file_url):
+    regex2 = re.compile('^https://imgur.com/a/([a-zA-Z0-9]*)$')
+    m = regex2.match(file_url)
+    if m:
+        print('This is an album! We are going to ignore these for now.')
+    else: # This is a single image
+        request_image(file_url + '.jpg')
 
-subreddit = reddit.subreddit('pythonforengineers')
-with open('out.txt', 'a') as f:
-    f.write(str(datetime.datetime.now()) + '\n')
+def request_image(file_url):
+    req = requests.get(file_url, stream=True, timeout=1.0)
+    regex_name = re.compile('.*/([a-zA-Z0-9]+(\.[a-zA-Z0-9]{3})?)$')
+    id = regex_name.match(file_url).group(1)
+    if req.status_code == requests.codes.ok:
+        with open('./img/' + id,'wb') as out_file:
+            shutil.copyfileobj(req.raw, out_file)
+    else:
+        req.raise_for_status()
+
+def main():
+    print('Authenticating...')
+    reddit = praw.Reddit('bot1')
+    print('Authentication successful!')
+    subreddit = reddit.subreddit('wallpapers')
+    regex = re.compile('^https://imgur.com/')
+    pathlib.Path('./img/').mkdir(parents=True, exist_ok=True)
     for submission in subreddit.top('day', limit=10):
-        f.write('https://www.reddit.com/' + submission.permalink + '\n')
-    f.write('\n')# Add a line between commits
+        print(submission.url)
+        if regex.match(submission.url):
+            request_imgur_image(submission.url)
+        else: # Direct image link
+           request_image(submission.url)
+
+main()
