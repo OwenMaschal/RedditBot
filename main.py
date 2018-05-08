163963bdacfd9ff7bf16a527e8043034075d4080
Owen Maschal <owenmaschal0598@gmail.com>
2018-05-08 16:10:55 -0400

    Added global vars which make customizing the bot easier.

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import praw
 import requests
 import shutil
 import re
-import json
 import pathlib
 import logging
 
-images_to_get = 10 # Number of images you want to download
+images_to_get = 10              # Number of images you want to download
+output_location = './img/'      # Image output location. Default: ./img/
+bot_name = 'bot1'               # Name of your bot definted in praw.ini
+subreddit_name = 'wallpapers'   # Name of subreddit to extract images from
+
 
 def request_imgur_image(file_url):
     regex2 = re.compile('^https://imgur.com/a/([a-zA-Z0-9]*)$')
@@ -21,7 +24,7 @@ def request_image(file_url):
     regex_name = re.compile('.*/([a-zA-Z0-9]+(\.[a-zA-Z0-9]{3})?)$')
     id = regex_name.match(file_url).group(1)
     if req.status_code == requests.codes.ok:
-        with open('./img/' + id,'wb') as out_file:
+        with open(output_location + id, 'wb') as out_file:
             shutil.copyfileobj(req.raw, out_file)
     else:
         req.raise_for_status()
@@ -35,11 +38,11 @@ def setup_praw_logging():
 
 def main():
     setup_praw_logging();
-    reddit = praw.Reddit('bot1')
-    subreddit = reddit.subreddit('wallpapers')
+    reddit = praw.Reddit(bot_name)
+    subreddit = reddit.subreddit(subreddit_name)
 
     regex = re.compile('^https://imgur.com/')
-    pathlib.Path('./img/').mkdir(parents=True, exist_ok=True)
+    pathlib.Path(output_location).mkdir(parents=True, exist_ok=True)
 
     for submission in subreddit.top('day', limit=images_to_get):
         if not submission.is_self: # Ignore self posts
