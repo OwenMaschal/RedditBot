e43d3fe89d5f90c596388e03833863078fe5486c
Owen Maschal <owenmaschal0598@gmail.com>
2018-05-08 21:07:29 -0400

    Added the 'urllib.request' library to simplify the image extraction.

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import praw
 import requests
-import shutil
 import re
 import pathlib
 import logging
+import urllib.request
 
-images_to_get = 10              # Number of images you want to download
+images_to_get = 50              # Number of images you want to download
 output_location = './img/'      # Image output location. Default: ./img/
 bot_name = 'bot1'               # Name of your bot definted in praw.ini
 subreddit_name = 'wallpapers'   # Name of subreddit to extract images from
+time_period = 'week'
 
-
-def request_imgur_image(file_url):
-    regex2 = re.compile('^https://imgur.com/a/([a-zA-Z0-9]*)$')
-    m = regex2.match(file_url)
-    if m: # Imgur Album Link
-        print('This is an album! This has not been implemented yet :(.')
-    else: # Imgur image link
-        request_image(file_url + '.jpg')
-
-def request_image(file_url):
-    req = requests.get(file_url, stream=True, timeout=10.0)
-    regex_name = re.compile('.*/([a-zA-Z0-9]+(\.[a-zA-Z0-9]{3})?)$')
-    id = regex_name.match(file_url).group(1)
-    if req.status_code == requests.codes.ok:
-        with open(output_location + id, 'wb') as out_file:
-            shutil.copyfileobj(req.raw, out_file)
-    else:
-        req.raise_for_status()
+# Check if the url has an extension
+# (Example: https://imgur.com/fa33.jpg matches)
+regex = re.compile('^.*\.[a-z]{3}$')
 
 def setup_praw_logging():
     handler = logging.StreamHandler()
@@ -36,19 +22,28 @@ def setup_praw_logging():
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
+def download_image(submission):
+    if not submission.is_self: # Ignore self posts
+        try:
+            if not regex.match(submission.url):
+                # If the link is missing a file extension, add it
+                submission.url = submission.url + '.jpg'
+            print(submission.id + ': ' +submission.url)
+            urllib.request.urlretrieve(submission.url, output_location + submission.id + '.jpg')
+        except Exception as e:
+            print(str(e) + ' at URL: ' + submission.url)
+
 def main():
+    # Setup logging and establish bot connection with Reddit
     setup_praw_logging();
     reddit = praw.Reddit(bot_name)
     subreddit = reddit.subreddit(subreddit_name)
 
-    regex = re.compile('^https://imgur.com/')
+    # Create an image folder if it doesn't already exist
     pathlib.Path(output_location).mkdir(parents=True, exist_ok=True)
 
-    for submission in subreddit.top('day', limit=images_to_get):
-        if not submission.is_self: # Ignore self posts
-            if regex.match(submission.url): # Is an Imgur link
-                request_imgur_image(submission.url)
-            else: # Direct image link (i.reddit)
-                request_image(submission.url)
+    # Get 'images_to_get' submissions and download the image from each
+    for submission in subreddit.top(time_period, limit=images_to_get):
+        download_image(submission)
 
 main()
